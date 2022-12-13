1698066d3d6c5bf913c32aba3ed711e0a282b0e2
Phazerous <nphazerous@gmail.com>
2022-12-14 01:40:48 +0600

    13. Roman to Integer | Initial

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
 "use strict";
-const maximumWealth = (accounts) => Math.max(...accounts.map((account) => account.reduce((acc, wealth) => acc + wealth, 0)));
-console.log(maximumWealth([
-    [2, 8, 7],
-    [7, 1, 3],
-    [1, 9, 5],
-]));
+const romanToInt = (str) => {
+    let sum = 0;
+    const sl = {
+        IV: 4,
+        IX: 9,
+        XL: 40,
+        XC: 90,
+        CD: 400,
+        CM: 900,
+    };
+    for (const key in sl) {
+        console.log(key);
+        if (str.includes(key)) {
+            str = str.replace(key, '');
+            sum += sl[key];
+        }
+    }
+    for (const char of str) {
+        switch (char) {
+            case 'I':
+                sum += 1;
+                break;
+            case 'V':
+                sum += 5;
+                break;
+            case 'X':
+                sum += 10;
+                break;
+            case 'L':
+                sum += 50;
+                break;
+            case 'C':
+                sum += 100;
+                break;
+            case 'D':
+                sum += 500;
+                break;
+            case 'M':
+                sum += 1000;
+                break;
+        }
+    }
+    return sum;
+};
+console.log(romanToInt('DCXXI'));
